bef0b717b2f96c8709e305f4e57f67fac95ba87b
Jacobo Jimenez <jacobo.jimenez@jambit.com>
2023-11-21 13:50:25 +0100

    bump telescope version, fix indent-blankline

--- nvim/after/plugin/indent_blankline.lua
+++ nvim/after/plugin/indent_blankline.lua
@@ -1,8 +1,8 @@
 -- vim.cmd [[highlight IndentBlanklineIndent guifg=#738091 gui=nocombine]]
 
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
+require('ibl').setup{
+  indent = { char = '┊' },
+  -- show_trailing_blankline_indent = false,
   -- char_highlight_list = {
   --   "IndentBlanklineInd1ent",
   -- },
