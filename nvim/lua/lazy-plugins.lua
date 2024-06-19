afaa2a210dfc51f23d44d506c80f11d3350d871b
Jacobo Jimenez <jacobo.jimenez@partner.bmw.de>
2024-06-19 11:30:54 +0200

    Add git integration with vim-fugitive

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -61,6 +61,8 @@ require("lazy").setup({
 
 	require("jjimenez/plugins/which-key"),
 
+	require("jjimenez/plugins/vim-fugitive"),
+
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
 	--
