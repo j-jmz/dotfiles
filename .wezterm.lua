e13c379bb76f108014da33e76ae2739334b9a705
Jacobo Jimenez <jacobo.jimenez@jambit.com>
2026-09-03 15:07:58 +0200

    add blur and adjust fonts for macos

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,14 +6,15 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font_with_fallback({ "Hack", "JetBrains Mono" })
-config.font_size = 12
+config.font = wezterm.font_with_fallback({ "Hack Nerd Font", "JetBrains Mono" })
+config.font_size = 14
 
 config.enable_tab_bar = false
 
 config.window_decorations = "TITLE | RESIZE"
 
 config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
 
 -- and finally, return the configuration to wezterm
 return config
